db488a9ca3592b6da5387769ca1bda5dfc6bb722
lishouxian <gzlishouxian@gmail.com>
2022-06-10 10:56:34 +0800

    提供转pb的方法

--- engines/predict.py
+++ engines/predict.py
@@ -13,6 +13,7 @@ class Predictor:
         self.device = device
         self.data_manager = data_manager
         self.logger = logger
+        self.checkpoints_dir = configs['checkpoints_dir']
         num_labels = len(self.data_manager.categories)
         if configs['model_type'] == 'bp':
             from engines.models.BinaryPointer import BinaryPointer
@@ -41,3 +42,22 @@ class Predictor:
         for class_id, result_set in results.items():
             results_dict[self.data_manager.reverse_categories[class_id]] = list(result_set)
         return results_dict
+
+    def convert_torch_to_tf(self):
+        import onnx
+        from onnx_tf.backend import prepare
+        max_sequence_length = self.data_manager.max_sequence_length
+        dummy_input = torch.ones([1, max_sequence_length]).to('cpu').long()
+        dummy_input = (dummy_input, dummy_input, dummy_input)
+        onnx_path = self.checkpoints_dir + '/model.onnx'
+        torch.onnx.export(self.model.to('cpu'), dummy_input, f=onnx_path, opset_version=10,
+                          input_names=['tokens', 'attentions', 'types'], output_names=['logits', 'probs'],
+                          do_constant_folding=False,
+                          dynamic_axes={'tokens': {0: 'batch_size'}, 'attentions': {0: 'batch_size'},
+                                        'types': {0: 'batch_size'}, 'logits': {0: 'batch_size'},
+                                        'probs': {0: 'batch_size'}})
+        model_onnx = onnx.load(onnx_path)
+        tf_rep = prepare(model_onnx)
+        pb_path = self.checkpoints_dir + '/model.pb'
+        tf_rep.export_graph(pb_path)
+        self.logger.info('convert torch to tensorflow pb successful...')
