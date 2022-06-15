22c0b37eab1d070137e52dc3a03e8fb8c78bd52b
Shaun <liuyang19@corp.netease.com>
2022-06-15 19:28:46 +0800

    change:补充一些日志

--- configure.py
+++ configure.py
@@ -10,7 +10,7 @@
 # test:跑测试集
 # convert2tf:将torch模型保存为tf框架的pb格式文件
 # [train, interactive_predict, test, convert2tf]
-mode = 'train'
+mode = 'test'
 
 # 使用GPU设备
 use_cuda = True
@@ -18,9 +18,9 @@ cuda_device = -1
 
 configure = {
     # 训练数据集
-    'train_file': 'data/example_datasets1/train_data.json',
+    'train_file': 'data/dataset/train_data.json',
     # 验证数据集
-    'dev_file': 'data/example_datasets1/dev_data.json',
+    'dev_file': 'data/dataset/dev_data.json',
     # 没有验证集时，从训练集抽取验证集比例
     'validation_rate': 0.15,
     # 测试数据集
@@ -28,13 +28,13 @@ configure = {
     # 使用的模型
     # bp: binary pointer
     # gp: global pointer
-    'model_type': 'bp',
+    'model_type': 'gp',
     # 模型保存的文件夹
-    'checkpoints_dir': 'checkpoints/example_datasets1',
+    'checkpoints_dir': 'checkpoints/20220615',
     # 模型名字
-    'model_name': 'best_model.pkl',
+    'model_name': 'model.pkl',
     # 类别列表
-    'classes': ['company', 'position', 'detail'],
+    'classes': ['PER', 'LOC', 'ORG'],
     # decision_threshold
     'decision_threshold': 0.5,
     # 是否使用苏神的多标签分类的损失函数，默认使用BCELoss
@@ -56,13 +56,13 @@ configure = {
     # 句子最大长度
     'max_sequence_length': 200,
     # epoch
-    'epoch': 50,
+    'epoch': 20,
     # batch_size
-    'batch_size': 16,
+    'batch_size': 32,
     # dropout rate
     'dropout_rate': 0.5,
     # 每print_per_batch打印损失函数
-    'print_per_batch': 100,
+    'print_per_batch': 200,
     # learning_rate
     'learning_rate': 5e-5,
     # 优化器选择
@@ -70,5 +70,5 @@ configure = {
     # 训练是否提前结束微调
     'is_early_stop': True,
     # 训练阶段的patient
-    'patient': 5,
+    'patient': 10,
 }
